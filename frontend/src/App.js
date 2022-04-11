07b0613835a73cd01a4bc350d7b3e2c19ce08923
Devin D <tui52164@gmail.com>
2022-04-11 12:56:23 -0400

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,25 @@
 import logo from './obeid_young.jpeg';
+import Home from './pages/home.js';
+import About from './pages/about.js';
+import SignUp from './pages/signup.js';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-        </p>
-        <p>
-          Welcome to Scruvy Mango
-          {/*Edit <code>src/App.js</code> and save to reload.*/}
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open App
-        </a>
-        <a
-          className="App-link2"
-          href="https://isip.piconepress.com/courses/temple/ece_3824/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here for fun
-        </a>
-      </header>
+      <div className="Navbar">
+        <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
