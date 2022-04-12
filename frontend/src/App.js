3b35ba9c970a4a7fa52a1aad77ad098f9beedd8e
Leo B <leo.battalora@gmail.com>
2022-04-12 23:17:12 +0000

    new app with supertokens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,39 @@
-import logo from './obeid_young.jpeg';
-import Home from './pages/home.js';
-import About from './pages/about.js';
-import SignUp from './pages/signup.js';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import { Routes, Route, useLocation } from 'react-router-dom';
+import * as reactRouterDom from "react-router-dom";
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
 
-function App() {
+SuperTokens.init({
+  appInfo: {
+    // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
+    appName: "Scurvy Mango",
+    apiDomain: "https://952f1871b9ba11ecafb6277fa4e3aa94-us-east-1.aws.supertokens.io:3568",
+    websiteDomain: "http://localhost:3000",
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
+  },
+  recipeList: [
+    EmailPassword.init(),
+    Session.init()
+  ]
+});
+
+export default function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <div className="Navbar">
-        <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/sign-up' element={<SignUp/>} />
-        </Routes>
-        </Router>
-      </div>
+    <div className="relative flex flex-col min-h-screen bg-prussian-blue text-white">
+      <Routes location={location} key={location.pathname}>
+        {/* This renders the login UI on the /auth route */}
+        {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
-
-export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center h-80 text-4xl font-serif">
+      Home
+    </div>
+  );
+}
