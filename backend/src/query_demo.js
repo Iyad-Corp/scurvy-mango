1ecc0ecf32d04dc80d1ed8a200c809ef077ce819
Iyad Obeid <iobeid@temple.edu>
2022-04-13 16:00:00 -0400

    added query_demo.js

---
+++ backend/src/query_demo.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://scurvy-mango:mango.scurvy@scurvy-mango.qpzfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+MongoClient.connect(uri, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Test");
+  
+    var query = { Name : "Steve Rogers" };
+    dbo.collection("data").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+}); 
+
+MongoClient.connect(uri, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Test");
+  
+    var query = { Name : "Google" };
+    dbo.collection("Companies").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+}); 
+
