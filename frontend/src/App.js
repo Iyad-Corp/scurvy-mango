f2bd4a79c659cc5d0402f4ccedd21b1ab73a5c88
Leo B <leo.battalora@gmail.com>
2022-04-13 17:36:06 +0000

    supertokens login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
+// file: src/App.js
+
+//=============================================================================
+// module imports
+//=============================================================================
+
+import { Routes, Route, useLocation } from 'react-router-dom';
+import * as reactRouterDom from "react-router-dom";
 import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
 import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import Session from "supertokens-auth-react/recipe/session";
-import { Routes, Route, useLocation } from 'react-router-dom';
-import * as reactRouterDom from "react-router-dom";
 import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
+
+//=============================================================================
+// initialize supertokens
+//=============================================================================
 
 SuperTokens.init({
   appInfo: {
     // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
     appName: "Scurvy Mango",
-    apiDomain: "https://952f1871b9ba11ecafb6277fa4e3aa94-us-east-1.aws.supertokens.io:3568",
+    apiDomain: "http://localhost:3001",
     websiteDomain: "http://localhost:3000",
     apiBasePath: "/auth",
     websiteBasePath: "/auth",
@@ -21,11 +31,15 @@ SuperTokens.init({
   ]
 });
 
-export default function App() {
+//=============================================================================
+// define app routes
+//=============================================================================
+
+function App() {
   const location = useLocation();
 
   return (
-    <div className="relative flex flex-col min-h-screen bg-prussian-blue text-white">
+    <div className="relative flex flex-col min-h-screen">
       <Routes location={location} key={location.pathname}>
         {/* This renders the login UI on the /auth route */}
         {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
@@ -37,3 +51,8 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
+
+//
+// end of file: src/App.js
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 export default function PageNotFound() {
   return (
-    <div className="flex flex-col justify-center items-center h-80 text-4xl font-serif">
+    <div className="flex flex-col justify-center items-center h-80 text-6xl font-serif">
       Home
     </div>
   );
