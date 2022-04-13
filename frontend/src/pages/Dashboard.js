54e0d80e38418bbb7b3e3dbb5647ef8690a60a31
Leo B <leo.battalora@gmail.com>
2022-04-13 19:42:54 +0000

    informational message

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -3,6 +3,8 @@ export default function Dashboard() {
     <div className="flex flex-col justify-center items-center h-80 space-y-5">
       <pre className="text-6xl font-serif">Dashboard</pre>
 
+      <pre>If you are on this page, it means that you are logged in.</pre>
+
       <a href='/' className="underline">
         Go back home.
       </a>
