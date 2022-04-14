c511760d6d660f5ba1001919d2237480c2ce4369
leo6liu <leo.battalora@gmail.com>
2022-04-14 04:07:12 -0400

    move constants to file

---
+++ backend/src/constants.js
@@ -0,0 +1,5 @@
+export const PORT = (process.env.PORT) ? process.env.PORT : 3001;
+export const BACKEND_URI = (process.env.BACKEND_URI) ? process.env.BACKEND_URI : 'http://localhost:3001';
+export const FRONTEND_URI = (process.env.FRONTEND_URI) ? process.env.FRONTEND_URI : 'http://localhost:3000';
+export const SUPERTOKENS_URI = process.env.SUPERTOKENS_URI;
+export const SUPERTOKENS_API_KEY = process.env.SUPERTOKENS_API_KEY;
